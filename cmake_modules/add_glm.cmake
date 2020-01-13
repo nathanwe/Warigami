ab763fa67a2e74232bac128371093933af09bcb3
concubicycle <saveliyb@gmail.com>
2020-01-13 11:48:30 -0800

    Added a

--- cmake_modules/add_glm.cmake
+++ cmake_modules/add_glm.cmake
@@ -2,8 +2,6 @@ include(FetchContent)
 
 set (GLM_TEST_ENABLE OFF CACHE INTERNAL "")
 
-set (CMAKE_CXX_FLAGS ${CXXFLAGS})
-
 FetchContent_Declare(glm
     PREFIX glm
     GIT_REPOSITORY https://github.com/g-truc/glm.git
--- cmake_modules/add_nlohmann_json.cmake
+++ cmake_modules/add_nlohmann_json.cmake
@@ -2,8 +2,6 @@ include(FetchContent)
 
 set (JSON_BuildTests OFF  CACHE INTERNAL "")
 
-set (CMAKE_CXX_FLAGS ${CXXFLAGS})
-
 FetchContent_Declare(nlohmann_json
     PREFIX nlohmann_json
     GIT_REPOSITORY https://github.com/nlohmann/json.git
