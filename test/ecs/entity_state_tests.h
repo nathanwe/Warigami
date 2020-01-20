9680175d64e9a0727aa81c3ebc702fad183e64c7
concubicycle <saveliyb@gmail.com>
2020-01-20 14:46:31 -0800

    Refactor ECS and project dependency structures.
    Refactor ECS so that there's a world class that contains the systems and state.
    
    Set up dependencies so that core is low level dependencies that other components will link to.

---
+++ test/ecs/entity_state_tests.h
@@ -0,0 +1,85 @@
+#include <cstdio>
+#include <cstring>
+
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <ecs/ecs_types.hpp>
+#include <ecs/world.hpp>
+
+#include "components.h"
+
+void test_basic();
+void test_each();
+
+
+int test_ecs_state_all()
+{
+    ecs::register_component<transform>(dummy_load);
+    ecs::register_component<renderdata>(dummy_load);
+
+    test_basic();
+    test_each();
+   
+    printf("Done with ecs state tests.\n");
+    return 0;
+}
+
+void test_basic()
+{
+    unsigned int  count = 100;
+
+    while (count-- > 0)
+    {
+        ecs::archetype_pools factory;
+        ecs::state state(factory);
+
+        auto& entityA = state.add_entity<transform, renderdata>();
+        auto& entityB = state.add_entity<transform>();
+
+        auto& transformA = entityA.get_component<transform>();
+        auto& transformB = entityB.get_component<transform>();
+        auto& renderA = entityA.get_component<renderdata>();
+    }
+}
+
+void test_each()
+{    
+    unsigned int  count = 100;
+
+    while (count-- > 0)
+    {
+        ecs::archetype_pools factory;
+        ecs::state state(factory);
+
+        auto& entityA = state.add_entity<transform, renderdata>();
+        auto& entityB = state.add_entity<transform, renderdata>();
+        auto& entityC = state.add_entity<transform>();
+
+        auto& transformA = entityA.get_component<transform>();
+        auto& transformB = entityB.get_component<transform>();
+        auto& renderA = entityA.get_component<renderdata>();
+
+        int iter_count = 0;
+
+        state.each<transform, renderdata>([&iter_count](transform& t, renderdata& r) {
+            iter_count++;
+            });
+
+        if (iter_count != 2) throw std::runtime_error("iteration is wrong");
+
+        iter_count = 0;
+
+        state.each<transform>([&iter_count](transform& t) {
+            iter_count++;
+            t.pitch = 123;
+            });
+
+        if (iter_count != 3) throw std::runtime_error("iteration is wrong");
+
+        state.each<transform>([&iter_count](transform& t) {
+            if (t.pitch != 123)
+                throw std::runtime_error("transform pitch values should all be 123");
+            });
+    }
+}
--- test/ecs/entity_world_tests.h
+++ test/ecs/entity_world_tests.h
@@ -6,80 +6,57 @@
 
 #include <ecs/ecs_types.hpp>
 #include <ecs/world.hpp>
+#include <ecs/system_base.hpp>
 
 #include "components.h"
 
-void test_basic();
-void test_each();
+class system_a : public ecs::system_base
+{
+public:
+	void update(ecs::state& state) override
+	{
+		update_count++;
+	}
 
+	int update_count = 0;
+};
+
+class system_b : public ecs::system_base
+{
+public:
+	void update(ecs::state& state) override
+	{
+		update_count++;
+	}
+
+	int update_count = 0;
+};
+
+void test_update();
 
 int test_ecs_world_all()
 {
-    ecs::register_component<transform>(dummy_load);
-    ecs::register_component<renderdata>(dummy_load);
-
-    test_basic();
-    test_each();
-   
-    printf("Done with ecs world tests.\n");
-    return 0;
+	test_basic();
+	printf("Done with ecs world tests.\n");
+	return 0;
 }
 
-void test_basic()
+void test_update()
 {
-    unsigned int  count = 100;
+	ecs::archetype_pools factory;
+	ecs::state state(factory);
 
-    while (count-- > 0)
-    {
-        ecs::archetype_pools factory;
-        ecs::world world(factory);
+	system_a a;
+	system_b b;
 
-        auto& entityA = world.add_entity<transform, renderdata>();
-        auto& entityB = world.add_entity<transform>();
+	ecs::systems systems ({ &a, &b });
 
-        auto& transformA = entityA.get_component<transform>();
-        auto& transformB = entityB.get_component<transform>();
-        auto& renderA = entityA.get_component<renderdata>();
-    }
-}
+	ecs::world world(systems, state);
 
-void test_each()
-{    
-    unsigned int  count = 100;
+	world.update();
 
-    while (count-- > 0)
-    {
-        ecs::archetype_pools factory;
-        ecs::world world(factory);
-
-        auto& entityA = world.add_entity<transform, renderdata>();
-        auto& entityB = world.add_entity<transform, renderdata>();
-        auto& entityC = world.add_entity<transform>();
-
-        auto& transformA = entityA.get_component<transform>();
-        auto& transformB = entityB.get_component<transform>();
-        auto& renderA = entityA.get_component<renderdata>();
-
-        int iter_count = 0;
-
-        world.each<transform, renderdata>([&iter_count](transform& t, renderdata& r) {
-            iter_count++;
-            });
-
-        if (iter_count != 2) throw std::runtime_error("iteration is wrong");
-
-        iter_count = 0;
-
-        world.each<transform>([&iter_count](transform& t) {
-            iter_count++;
-            t.pitch = 123;
-            });
-
-        if (iter_count != 3) throw std::runtime_error("iteration is wrong");
-
-        world.each<transform>([&iter_count](transform& t) {
-            if (t.pitch != 123)
-                throw std::runtime_error("transform pitch values should all be 123");
-            });
-    }
+	if (a.update_count != 0 && b.update_count != 0)
+	{
+		throw std::runtime_error("failed to update...?");
+	}
 }
