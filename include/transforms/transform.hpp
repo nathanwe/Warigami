31972a6ae93cdfd1b205e8b75581cc3704eb5a2e
Nicolas San Jose <nicolas.a.sanjose@gmail.com>
2020-01-23 18:48:51 -0800

    fixed transform component bugs
    added transform has_parent field to prevent bug where parent's id might be recycled and be valid when checked for existence
    changed default scale to 1

--- include/transforms/transform.hpp
+++ include/transforms/transform.hpp
@@ -11,16 +11,16 @@ namespace transforms
 {
 	struct transform : ecs::component<transform>
 	{
-		entity_id parent;
 		glm::vec3 position       = glm::vec3(0);
 		glm::vec3 rotation       = glm::vec3(0);
-		glm::vec3 scale          = glm::vec3(0);
+		glm::vec3 scale          = glm::vec3(1);
 		glm::mat4 local_to_world = glm::mat4(0);
 		bool is_matrix_dirty     = true;
+		entity_id parent         = 0;
+		bool has_parent          = false;
 	};
 }
 
 template<> const component_shift ecs::component<transforms::transform>::component_bitshift = (component_shift)transforms::component_bits::transform;
-template<> const component_bitset ecs::component<transforms::transform>::archetype_bit = component_bitset(1) << transforms::transform::component_bitshift;
 
 #endif
