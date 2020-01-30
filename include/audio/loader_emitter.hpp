ce6e828800a900d843d4fba6fbc2f4d82b45a4f9
saveliy.baranov <saveliy.baranov@digipen.edu>
2020-01-29 23:39:27 -0800

    add the loader for sound emitters.
    sound listeners may not even need one for now. or maybe i'm just lazy

---
+++ include/audio/loader_emitter.hpp
@@ -0,0 +1,12 @@
+//
+// Created by sava on 1/29/20.
+//
+
+#ifndef WIZARDENGINE_AUDIO_LOADER_HPP
+#define WIZARDENGINE_AUDIO_LOADER_HPP
+
+#include <asset/component_loader.hpp>
+
+
+
+#endif //WIZARDENGINE_AUDIO_LOADER_HPP
