d054023ad2d219004e50702da9d6b94b5dfa33ab
saveliy.baranov <saveliy.baranov@digipen.edu>
2020-01-30 17:38:19 -0800

    fixed segfault on demo close
    glfwTerminate needs to run after all the systems are torn down, which won't be the case if it's called from main()

--- apps/demo/main.cpp
+++ apps/demo/main.cpp
@@ -20,6 +20,8 @@
 
 #include "fly_cam_system.hpp"
 
+void run_game(GLFWwindow* window, uint32_t window_width, uint32_t window_height, bool is_debug);
+
 int main(int argc, char** argv)
 {
 #ifndef NDEBUG
@@ -61,51 +63,56 @@ int main(int argc, char** argv)
 	}
 	glfwMakeContextCurrent(window);
 
-	rendering::viewport window_view{ 0, 0, window_width, window_height };
-	
-	core::input_manager input(window);
-	core::frame_timer timer;
-
-	// init ecs state
-	ecs::archetype_pools memory;
-	ecs::state state(memory);
-	ecs::register_component<transforms::transform>("transform");
-	ecs::register_component<rendering::camera>("camera");
-	ecs::register_component<rendering::light_directional>("light_directional");
-	ecs::register_component<rendering::light_point>("light_point");
-	ecs::register_component<rendering::renderable_mesh_static>("renderable_mesh_static");
-	rendering::asset_cache render_asset_cache;
-
-	rendering::renderer renderer(window, window_view, is_debug);
-	transforms::transformer transformer;
-	rendering::camera_updater camera_updater;
-	fly_cam flycam(input, timer);
-	ecs::systems systems({ &transformer, &camera_updater, &renderer, &flycam });
-	ecs::world world(systems, state);
-
-	asset::json_cache cache;
-	asset::scene scene("assets/scenes/scene.json", cache);
-	asset::scene_hydrater hydrater(state, scene);
-	transforms::transform_loader transform_loader;
-	rendering::loader_camera camera_loader(render_asset_cache);
-	rendering::loader_light_directional dir_light_loader;
-	rendering::loader_light_point point_light_loader;
-	rendering::render_loader render_loader(render_asset_cache);
-	hydrater.register_loaders(&transform_loader, &camera_loader, &dir_light_loader, &point_light_loader, &render_loader);
-	hydrater.load();
-
-	//game loop
-	while (!glfwWindowShouldClose(window))
-	{
-		timer.start();
-		glfwPollEvents();
-		input.update();
-		if (input.was_key_pressed(GLFW_KEY_ESCAPE)) {
-			glfwWindowShouldClose(window);
-		}
-		world.update();
-		timer.end();
-	}
+    run_game(window, window_width, window_height, is_debug);
 
 	glfwTerminate();
+}
+
+void run_game(GLFWwindow* window, uint32_t window_width, uint32_t window_height, bool is_debug)
+{
+    rendering::viewport window_view{ 0, 0, window_width, window_height };
+
+    core::input_manager input(window);
+    core::frame_timer timer;
+
+    // init ecs state
+    ecs::archetype_pools memory;
+    ecs::state state(memory);
+    ecs::register_component<transforms::transform>("transform");
+    ecs::register_component<rendering::camera>("camera");
+    ecs::register_component<rendering::light_directional>("light_directional");
+    ecs::register_component<rendering::light_point>("light_point");
+    ecs::register_component<rendering::renderable_mesh_static>("renderable_mesh_static");
+    rendering::asset_cache render_asset_cache;
+
+    rendering::renderer renderer(window, window_view, is_debug);
+    transforms::transformer transformer;
+    rendering::camera_updater camera_updater;
+    fly_cam flycam(input, timer);
+    ecs::systems systems({ &transformer, &camera_updater, &renderer, &flycam });
+    ecs::world world(systems, state);
+
+    asset::json_cache cache;
+    asset::scene scene("assets/scenes/scene.json", cache);
+    asset::scene_hydrater hydrater(state, scene);
+    transforms::transform_loader transform_loader;
+    rendering::loader_camera camera_loader(render_asset_cache);
+    rendering::loader_light_directional dir_light_loader;
+    rendering::loader_light_point point_light_loader;
+    rendering::render_loader render_loader(render_asset_cache);
+    hydrater.register_loaders(&transform_loader, &camera_loader, &dir_light_loader, &point_light_loader, &render_loader);
+    hydrater.load();
+
+    //game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        timer.start();
+        glfwPollEvents();
+        input.update();
+        if (input.was_key_pressed(GLFW_KEY_ESCAPE)) {
+            glfwWindowShouldClose(window);
+        }
+        world.update();
+        timer.end();
+    }
 }
