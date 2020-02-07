b74fbd2c7b101c3f90dad03c925619489a33b9f1
concubicycle <saveliyb@gmail.com>
2020-02-07 03:48:51 -0800

    event class

--- include/core/console_command_event.hpp
+++ include/core/console_command_event.hpp
@@ -1,11 +1,20 @@
+#ifndef __CONSOLE_COMMAND_EVENT_HPP_
+#define __CONSOLE_COMMAND_EVENT_HPP_
+
+#include <string>
+
+#include <event/event.hpp>
+
 namespace core
 {
-	class console_command_event 
+	class console_command_event : public Event;
 	{
 	public:
-
-
-	private:
-
+		console_command_event(std::string c) : command(c) {}	
+		const std::string command;
 	};
 }
+
+
+
+#endif
