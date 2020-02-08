7af6e1a67879ddb082032f1697ad16d9b56b0737
concubicycle <saveliyb@gmail.com>
2020-02-08 02:45:31 -0800

    better positioning

--- src/engine-ui/developer_console.cpp
+++ src/engine-ui/developer_console.cpp
@@ -13,9 +13,14 @@ engineui::developer_console::developer_console(core::viewport& viewport, EventMa
 
 void engineui::developer_console::draw()
 {
-    ImGui::SetNextWindowSize({ (float)_viewport.width, Height}, ImGuiCond_FirstUseEver);
-    ImGui::SetNextWindowPos({ 0, (float)_viewport.height - Height }, ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize({ (float)_viewport.width, Height }, ImGuiCond_Always);
     ImGui::Begin("DevConsole");
+    if (ImGui::IsWindowCollapsed())        
+        ImGui::SetWindowPos({ 0, (float)_viewport.height - ImGui::GetWindowHeight() }, ImGuiCond_Always);
+    else        
+        ImGui::SetWindowPos({ 0, (float)_viewport.height - Height }, ImGuiCond_Always);
+    
+
     ImGui::PushItemWidth(-1);
     ImGui::InputTextMultiline("##out", _output, IM_ARRAYSIZE(_output), { 0, Height - 64 }, ImGuiInputTextFlags_ReadOnly);
     ImGui::InputText("##in", _input, IM_ARRAYSIZE(_input));
