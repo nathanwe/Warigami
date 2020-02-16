5fd263f764d5d8515fbc9b024e01056888990d3e
concubicycle <saveliyb@gmail.com>
2020-02-16 03:27:05 -0800

    add index header

--- apps/demo/main.cpp
+++ apps/demo/main.cpp
@@ -1,34 +1,4 @@
-// Engine systems
-#include "asset/json_cache.hpp"
-#include "asset/scene.hpp"
-#include "asset/scene_hydrater.hpp"
-#include "core/viewport.hpp"
-#include <core/glfw_context.hpp>
-#include "ecs/register_component.hpp"
-#include "ecs/state.hpp"
-#include "ecs/world.hpp"
-#include "transforms/transform.hpp"
-#include "transforms/transformer.hpp"
-#include "transforms/transform_loader.hpp"
-#include "rendering/camera.hpp"
-#include "rendering/camera_updater.hpp"
-#include "rendering/debug_view.hpp"
-#include "rendering/light_directional.hpp"
-#include "rendering/light_point.hpp"
-#include "rendering/loader_camera.hpp"
-#include "rendering/loader_light_directional.hpp"
-#include "rendering/loader_light_point.hpp"
-#include "rendering/render_loader.hpp"
-#include "rendering/renderable_mesh_static.hpp"
-#include "rendering/renderer.hpp"
-#include "collisions/collider.hpp"
-#include "collisions/aabb_collider_loader.hpp"
-#include "collisions/sphere_collider_loader.hpp"
-#include "collisions/rigid_body_loader.hpp"
-#include "collisions/collision_manager.hpp"
-#include <audio/audio_system.hpp>
-#include <audio/loader_emitter.hpp>
-#include <physics/physics_update.hpp>
+#include <engine.hpp>
 
 // Components
 #include "components/game_piece.hpp"
@@ -50,6 +20,7 @@
 #include "box_move_system.hpp"
 #include "util/boardgen.hpp"
 
+// Overlays
 #include <engine-ui/imgui_overlay.hpp>
 #include <engine-ui/developer_console.hpp>
 #include <engine-ui/entities_view.hpp>
---
+++ include/engine.hpp
@@ -0,0 +1,33 @@
+// Index header, which includes common engine headers for convenience. 
+
+// Engine Systems
+#include <asset/json_cache.hpp>
+#include <asset/scene_hydrater.hpp>
+#include <asset/scene.hpp>
+#include <audio/audio_system.hpp>
+#include <audio/loader_emitter.hpp>
+#include <collisions/aabb_collider_loader.hpp>
+#include <collisions/collider.hpp>
+#include <collisions/collision_manager.hpp>
+#include <collisions/rigid_body_loader.hpp>
+#include <collisions/sphere_collider_loader.hpp>
+#include <core/glfw_context.hpp>
+#include <core/viewport.hpp>
+#include <ecs/register_component.hpp>
+#include <ecs/state.hpp>
+#include <ecs/world.hpp>
+#include <physics/physics_update.hpp>
+#include <rendering/camera_updater.hpp>
+#include <rendering/camera.hpp>
+#include <rendering/debug_view.hpp>
+#include <rendering/light_directional.hpp>
+#include <rendering/light_point.hpp>
+#include <rendering/loader_camera.hpp>
+#include <rendering/loader_light_directional.hpp>
+#include <rendering/loader_light_point.hpp>
+#include <rendering/render_loader.hpp>
+#include <rendering/renderable_mesh_static.hpp>
+#include <rendering/renderer.hpp>
+#include <transforms/transform_loader.hpp>
+#include <transforms/transform.hpp>
+#include <transforms/transformer.hpp>
