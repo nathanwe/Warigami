4df9c5222d32b07d9f308e49771c00d3993f1244
saveliyb <saveliyb@gmail.com>
2020-02-28 20:01:45 -0800

    format boardgen

--- apps/boardgen/main.cpp
+++ apps/boardgen/main.cpp
@@ -25,40 +25,40 @@ int main()
     };
 
     for (int i = 0; i < 6; ++i )
-        for (int j = 0; j < 8; ++j )
+    {
+        for (int j = 0; j < 8; ++j)
         {
             std::string boardsquare_proto = "invalid";
 
-            if (i%2 != 0)
-                if (j%2 == 0)
+            if (i % 2 != 0)
+                if (j % 2 == 0)
                     boardsquare_proto = "assets/prototypes/boardsquare.json";
                 else
                     boardsquare_proto = "assets/prototypes/boardsquare2.json";
+            else if (j % 2 != 0)
+                boardsquare_proto = "assets/prototypes/boardsquare.json";
             else
-                if (j%2 != 0)
-                    boardsquare_proto = "assets/prototypes/boardsquare.json";
-                else
-                    boardsquare_proto = "assets/prototypes/boardsquare2.json";
+                boardsquare_proto = "assets/prototypes/boardsquare2.json";
 
 
             all["children"][i + j] = {
-                    {"prototype", boardsquare_proto},
-                    {"components", {
-                           {
-                               {"type", "transform"},
-                               {"position", {i - 3, 1, j - 4}}
-                           },
-                           {
-                               {"type", "board_square"},
-                               {"x", i},
-                               {"y", j}
-                           }
-                    }},
+                {"prototype",  boardsquare_proto},
+                {"components", {
+                   {
+                       {"type", "transform"},
+                       {"position", {i - 3, 1, j - 4}}
+                   },
+                   {
+                       {"type", "board_square"},
+                       {"x", i},
+                       {"y", j}
+                   }
+               }},
             };
         }
+    }
 
     std::ofstream o("gameboard.json");
     o << all << std::endl;
-
     return 0;
 }
