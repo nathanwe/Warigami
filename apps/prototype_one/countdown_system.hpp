90ebabc8a010889afa1aff432291a274084fabb9
Nicolas San Jose <nicolas.a.sanjose@gmail.com>
2020-04-14 15:38:08 -1000

    fix countdown not showing "Go!"

--- apps/prototype_one/countdown_system.hpp
+++ apps/prototype_one/countdown_system.hpp
@@ -59,43 +59,58 @@ public:
 	void update(ecs::state& r_state) override
 	{		
 		if (_board == nullptr) return;
-
 		auto& board_component = _board->get_component<components::board>();
 
 		// Count before starting (3, 2, 1, GO!)
-		if (board_component.state == components::game_state::countdown) {
-			r_state.each_id<components::countdown, rendering::renderable_text>([&](auto id, auto& countdown, rendering::renderable_text& text)
-			{
-				if (countdown.current_value <= 0) {
-					// Prepare for the real countdown
-					countdown.current_value = countdown.count_duration;
-					text.position = glm::ivec2(_glfw_context.width() / 2 - 60, _glfw_context.height() - 60);
-					text.scale = 1.5;
-					is_start_count = false;
-					game_start_event gs;
-					event_manager.BroadcastEvent(gs);
-					std::cerr << "Game Start!" << std::endl;
-				}
-				else if (countdown.current_value < 1) {
-					// Show "GO!"
-					text.position = glm::ivec2(_glfw_context.width() / 2 - 30*text.scale, _glfw_context.height() / 2);
-					text.scale += 0.02;
-					text.string_hash = _go_string;
-					board_component.state = components::game_state::gameplay;
-					r_state.each<components::pause>([&](auto& pause)
-						{
-							pause.is_game_countdown_over = true;
-						});
-					r_state.remove_entity(r_state.find_entity(id));
-				}
-				else {
-					text.scale += 0.01;
-					std::ostringstream oss;
-					oss << (int)countdown.current_value << "!";
-					text.string_hash = _strings.hash_and_store(oss.str());
-				}
-				countdown.current_value -= m_timer.delta_secs();
-			});
+		if (board_component.state == components::game_state::countdown) 
+		{
+			r_state.each_id<components::countdown, rendering::renderable_text>(
+				[&](auto id, auto& countdown, rendering::renderable_text& text)
+				{
+					if (countdown.current_value <= 0) 
+					{
+						// Prepare for the real countdown
+						countdown.current_value = countdown.count_duration;
+						text.position = glm::ivec2(_glfw_context.width() / 2 - 60, _glfw_context.height() - 60);
+						text.scale = 1.5;
+						is_start_count = false;
+						game_start_event gs;
+						event_manager.BroadcastEvent(gs);
+						std::cerr << "Game Start!" << std::endl;
+					}
+					else if (countdown.current_value < 1) 
+					{
+						// Show "GO!"
+						text.position = glm::ivec2(_glfw_context.width() / 2 - 30*text.scale, _glfw_context.height() / 2);
+						text.scale += 0.02;
+						text.string_hash = _go_string;
+						board_component.state = components::game_state::gameplay;
+						r_state.each<components::pause>([&](auto& pause)
+							{
+								pause.is_game_countdown_over = true;
+							});
+					}
+					else 
+					{
+						text.scale += 0.01;
+						std::ostringstream oss;
+						oss << (int)countdown.current_value << "!";
+						text.string_hash = _strings.hash_and_store(oss.str());
+					}
+					countdown.current_value -= m_timer.delta_secs();
+				});
+		}
+		else if (board_component.state == components::game_state::gameplay)
+		{
+			r_state.each_id<components::countdown, rendering::renderable_text>(
+				[&](auto id, auto& countdown, rendering::renderable_text& text)
+				{
+					countdown.current_value -= m_timer.delta_secs();
+					if (countdown.current_value <= 0)
+					{
+						r_state.remove_entity(r_state.find_entity(id));
+					}
+				});
 		}
 	}
 
