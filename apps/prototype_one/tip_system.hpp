a0b21bea83aad6b8791becd60093fee36626d101
nathan <mtg@weises.org>
2020-04-18 16:14:11 -0700

    vanishes when you leave

--- apps/prototype_one/tip_system.hpp
+++ apps/prototype_one/tip_system.hpp
@@ -37,22 +37,24 @@ public:
             [&](entity_id board_id, transforms::transform& board_t, components::board& board) {
                 if (board.state != components::game_state::gameplay) return;
 
+                
+
                 r_state.each_id<transforms::transform, components::tip, rendering::renderable_mesh_static>(
                     [&](entity_id tip_id, transforms::transform& tip_t, components::tip& tip, rendering::renderable_mesh_static& tip_r) {
+                        tip_r.is_enabled = false;
                         r_state.each_id<transforms::transform, components::board_square, components::terrain>(
                             [&](entity_id board_square_id, transforms::transform& square_t, components::board_square& square, components::terrain& terrain) {
                                 r_state.each_id<components::player>(
                                     [&](entity_id player_id,  components::player& player) {
-                                        if (player.has_ever_gained_energy && player.team == tip.team) {
-                                            tip_r.is_enabled = false;
-                                        }
-                                        if ((terrain.growth_stage == 3 || terrain.growth_stage == 2)
-                                            && (tip.team == square.team) && (player.selected_row == square.x)
-                                            && (player.team == tip.team))
-                                        {
-                                            tip_t.position = board.grid_to_board(glm::vec2(square.x, square.y), board_t);
-                                            tip_t.is_matrix_dirty = true;
-                                        }
+                                            if ((terrain.growth_stage == 3 || terrain.growth_stage == 2)
+                                                && (tip.team == square.team) && (player.selected_row == square.x)
+                                                && (player.team == tip.team)
+                                                && !(player.has_ever_gained_energy))
+                                            {
+                                                tip_r.is_enabled = true;
+                                                tip_t.position = board.grid_to_board(glm::vec2(square.x, square.y), board_t);
+                                                tip_t.is_matrix_dirty = true;
+                                            }
                                         
                                     });
                             });
