dc3573396c9282735e6f5ccff8cb8f4def6eca94
nathan <mtg@weises.org>
2020-04-18 16:26:53 -0700

    more fixes

--- apps/prototype_one/spiderling_system.hpp
+++ apps/prototype_one/spiderling_system.hpp
@@ -77,9 +77,9 @@ private:
 						piece.health_points[piece.health].get_component<rendering::renderable_mesh_static>().material.param_diffuse = glm::vec3(1, 0, 0);
 					}
 				}
-				else if (open_space[1] && piece.spider_egg_spawn_counter == 1)
+				else if (open_space[1] && piece.spider_egg_spawn_counter == 2)
 				{
-					to_spawn new_spawn(piece.board_source.x - 1, piece.board_source.y, piece.team, components::card_enum::SCISSORLING);
+					to_spawn new_spawn(piece.board_source.x + 1, piece.board_source.y, piece.team, components::card_enum::SCISSORLING);
 					board.spawner.push_back(new_spawn);
 					piece.health -= 1;
 					// Turn health spheres red
@@ -87,9 +87,9 @@ private:
 						piece.health_points[piece.health].get_component<rendering::renderable_mesh_static>().material.param_diffuse = glm::vec3(1, 0, 0);
 					}
 				}
-				else if (open_space[2] && piece.spider_egg_spawn_counter == 2)
+				else if (open_space[2] && piece.spider_egg_spawn_counter == 1)
 				{
-					to_spawn new_spawn(piece.board_source.x + 1, piece.board_source.y, piece.team, components::card_enum::SCISSORLING);
+					to_spawn new_spawn(piece.board_source.x - 1, piece.board_source.y, piece.team, components::card_enum::SCISSORLING);
 					board.spawner.push_back(new_spawn);
 					piece.health -= 1;
 					// Turn health spheres red
